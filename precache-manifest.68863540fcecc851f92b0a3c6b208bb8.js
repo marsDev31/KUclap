e912034cbc6b6a5450e15f27b8949f7b05815947
poonsht <poon_arsene_lupin@hotmail.com>
2020-04-27 13:54:29 +0700

    Updates

---
+++ precache-manifest.68863540fcecc851f92b0a3c6b208bb8.js
@@ -0,0 +1,65 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/KUclap/af138d6894aafa187b1c4999d83577c9.png"
+  },
+  {
+    "revision": "9787390d11e47bc036be53c1f84345d2",
+    "url": "/KUclap/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "aec9f58c5b40b65c5582a4029724c775",
+    "url": "/KUclap/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b7531f2c8e91281da44f7afae2de110d",
+    "url": "/KUclap/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4f0d889b937b2cd23368e2849b9fbcbc",
+    "url": "/KUclap/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "dd7fc04f4a7093bd0c3aac1eeb6d7a09",
+    "url": "/KUclap/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "af138d6894aafa187b1c4999d83577c9",
+    "url": "/KUclap/assets/icons/marsdev31.png"
+  },
+  {
+    "revision": "830e8e57d8f85e6fdfbb4ac91e8e14fc",
+    "url": "/KUclap/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e2e8d3bde67307537393",
+    "url": "/KUclap/bundle.e2e8d.js"
+  },
+  {
+    "revision": "b6dba16d0ff4deaa4920b52c4bd4d24f",
+    "url": "/KUclap/index.html"
+  },
+  {
+    "revision": "1c45d68225e51222852a",
+    "url": "/KUclap/polyfills.1c45d.js"
+  },
+  {
+    "revision": "b05074cc0a77d1c98d5b",
+    "url": "/KUclap/route-Home.chunk.b0507.js"
+  },
+  {
+    "revision": "b05074cc0a77d1c98d5b",
+    "url": "/KUclap/route-Home.chunk.d8f82.css"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/KUclap/sw-debug.js"
+  },
+  {
+    "revision": "8f14f47a53be37b9e4fc9e7901418545",
+    "url": "/KUclap/sw-esm.js"
+  },
+  {
+    "revision": "d49bf5602d2eea483c694648ca78aee2",
+    "url": "/KUclap/sw.js"
+  }
+]);
---
+++ precache-manifest.ad7a77565303902cfa1f2da587a70340.esm.js
@@ -0,0 +1,53 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/KUclap/af138d6894aafa187b1c4999d83577c9.png"
+  },
+  {
+    "revision": "9787390d11e47bc036be53c1f84345d2",
+    "url": "/KUclap/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "aec9f58c5b40b65c5582a4029724c775",
+    "url": "/KUclap/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b7531f2c8e91281da44f7afae2de110d",
+    "url": "/KUclap/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4f0d889b937b2cd23368e2849b9fbcbc",
+    "url": "/KUclap/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "dd7fc04f4a7093bd0c3aac1eeb6d7a09",
+    "url": "/KUclap/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "af138d6894aafa187b1c4999d83577c9",
+    "url": "/KUclap/assets/icons/marsdev31.png"
+  },
+  {
+    "revision": "830e8e57d8f85e6fdfbb4ac91e8e14fc",
+    "url": "/KUclap/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e2e8d3bde67307537393",
+    "url": "/KUclap/bundle.e3b6f.esm.js"
+  },
+  {
+    "revision": "b6dba16d0ff4deaa4920b52c4bd4d24f",
+    "url": "/KUclap/index.html"
+  },
+  {
+    "revision": "1c45d68225e51222852a",
+    "url": "/KUclap/polyfills.7a9c4.esm.js"
+  },
+  {
+    "revision": "b05074cc0a77d1c98d5b",
+    "url": "/KUclap/route-Home.chunk.2dfcd.esm.js"
+  },
+  {
+    "revision": "b05074cc0a77d1c98d5b",
+    "url": "/KUclap/route-Home.chunk.d8f82.css"
+  }
+]);
